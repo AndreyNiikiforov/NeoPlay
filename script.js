a81fc5d08ea0dca3d6b495dca0dade446501434f
AndreyNiikiforov <andreynikiforov2809@gmail.com>
2026-06-15 17:37:42 +0500

    Add files via upload
    Сайт начинает работу

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("NeoPlay загружен");
+
+// Покупка билета
+document.getElementById("buyTicketBtn")?.addEventListener("click", function(e) {
+    e.preventDefault();
+    alert("Оплата через ЮMoney скоро будет доступна. А пока напиши @neoplay_support для покупки билета.");
+});
+
+document.getElementById("giveawayBtn")?.addEventListener("click", function(e) {
+    e.preventDefault();
+    alert("Оплата через ЮMoney скоро будет доступна. А пока напиши @neoplay_support для покупки билета.");
+});
